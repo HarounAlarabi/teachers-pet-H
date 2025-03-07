63bb5cf811ed3d3c8d44eba629565e9b7c0eed6b
Haroun Alarabi <alarabiharoun@gmail.com>
2025-03-07 00:58:09 +0000

    update

--- server/db-client.js
+++ server/db-client.js
@@ -1,18 +1,28 @@
 const { Client } = require("pg");
 
-const client = new Client({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  ssl: {
-    rejectUnauthorized: true,
-  },
-});
+const connectionConfig = process.env.DATABASE_URL
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    };
 
-client.connect(function (err) {
-  if (err) throw err;
-});
+const client = new Client(connectionConfig);
+
+client
+  .connect()
+  .then(() => console.log("PostgreSQL connected"))
+  .catch((err) => console.error("Connection error:", err.stack));
 
 module.exports = client;
