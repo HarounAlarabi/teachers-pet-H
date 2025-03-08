181df91022d557c234505288625fed6768525d5a
Haroun Alarabi <alarabiharoun@gmail.com>
2025-03-08 22:49:15 +0000

    fiexd bug

--- server/server.js
+++ server/server.js
@@ -44,9 +44,24 @@ const express = require("express");
 const app = express();
 let cors = require("cors");
 
-app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.options("/login", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.sendStatus(204);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
