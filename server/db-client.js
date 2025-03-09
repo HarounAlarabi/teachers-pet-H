535d1afcb29af5fd64b1718ccd0a777e2a558762
Haroun Alarabi <109032469+HarounAlarabi@users.noreply.github.com>
2025-03-09 00:06:48 +0000

    Update db-client.js

--- server/db-client.js
+++ server/db-client.js
@@ -1,4 +1,5 @@
-const { Client } = require('pg');
+require("dotenv").config();
+const { Client } = require("pg");
 
 const client = new Client({
   host: process.env.DB_HOST,
@@ -6,18 +7,13 @@ const client = new Client({
   database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+
   ssl: {
     rejectUnauthorized: false,
   },
 });
 
-client.connect()
-  .then(() => {
-    console.log("Connected to PostgreSQL!");
-  })
-  .catch((err) => {
-    console.error("Database connection failed:", err.message);
-    process.exit(1);
-  });
+client
+  .connect()
 
-module.exports = client;
+  .then(() => console.log("Connected to database"));
