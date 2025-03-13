fc60107c93650a8948d435ea96c6259d26767679
Haroun Alarabi <alarabiharoun@gmail.com>
2025-03-13 02:17:33 +0000

    updat style

--- server/fetchPupilData.js
+++ server/fetchPupilData.js
@@ -3,31 +3,27 @@ const client = require("./db-client");
 function fetchPupilData(req, res) {
   const reqTeacherID = req.body.teacherID;
   client
-      .query(
-        "select pv.pupil_id, pv.pupil_nickname, pv.last_update_date, pv.override_score, pv.calculated_score, case when pv.override_score is null then sam.support_category else pv.override_support_category end as final_support_category, case when pv.override_score is null then sam.support_allocation_text else pv.override_support_allocation_text end as final_support_allocation from pupil_view pv left join support_allocation_matrix sam on (pv.calculated_score >= sam.range_minimum and pv.calculated_score <= range_maximum) where pv.teacher_id = $1", [reqTeacherID]
-      )
-      .then((result) => {
-        if (result.rowCount > 0) {
-          res.status(200).json(result.rows);
-        } else {
-          res.status(404).json({
-            result: "failure",
-            message: "Pupil summary list could not be found",
-          });
-        }
-      })
-      .catch((error) => {
-        console.error(error.message);
-        res
-          .status(502)
-          .json({
-            result: "failure",
-            message: "Error fetching pupil data",
-          })
-          .finally(() => {
-            client.end();
-          });
+    .query(
+      "select pv.pupil_id, pv.pupil_nickname, pv.last_update_date, pv.override_score, pv.calculated_score, case when pv.override_score is null then sam.support_category else pv.override_support_category end as final_support_category, case when pv.override_score is null then sam.support_allocation_text else pv.override_support_allocation_text end as final_support_allocation from pupil_view pv left join support_allocation_matrix sam on (pv.calculated_score >= sam.range_minimum and pv.calculated_score <= range_maximum) where pv.teacher_id = $1",
+      [reqTeacherID]
+    )
+    .then((result) => {
+      if (result.rowCount > 0) {
+        res.status(200).json(result.rows);
+      } else {
+        res.status(404).json({
+          result: "failure",
+          message: "Pupil summary list could not be found",
+        });
+      }
+    })
+    .catch((error) => {
+      console.error(error.message);
+      res.status(502).json({
+        result: "failure",
+        message: "Error fetching pupil data",
       });
+    });
 }
 
 module.exports = fetchPupilData;
--- server/save-user-form-input.js
+++ server/save-user-form-input.js
@@ -1,7 +1,8 @@
 const client = require("./db-client");
+const deletePupil = require("./delete-pupil");
 
 const saveUserFormInput = (req, res) => {
-  const { formSubmission } = req.body || {};
+  const { formSubmission } = req.body;
   const {
     teacherID,
     pupilID,
@@ -12,83 +13,131 @@ const saveUserFormInput = (req, res) => {
     teacherSelectedAnswers,
   } = formSubmission;
 
-  // Track if response has been sent
-  let responseSent = false;
-
   // this function will add an individual answer to the table
   function addAnAnswer(pupilID, anAnswer) {
-    return client.query(
-      "INSERT INTO selected_option (pupil_id, answer_id, teacher_comment) VALUES ($1,$2,$3)",
-      [pupilID, anAnswer.answerID, anAnswer.teacherComment]
-    );
+    client
+      .query(
+        "INSERT INTO selected_option (pupil_id, answer_id, teacher_comment) VALUES ($1,$2,$3)",
+        [pupilID, anAnswer.answerID, anAnswer.teacherComment]
+      )
+      .then((result) => {
+        if (result.rowCount > 0) {
+        } else {
+          res.status(404).json({
+            result: "failure",
+            message: "teacher answer could not be inserted",
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+        res.status(502).json({
+          result: "failure",
+          message: "Error saving teacher answers",
+        });
+        client.end();
+      });
   }
 
   // delete existing pupil record before inserting new version
-  const deleteExistingRecords = () => {
-    if (!pupilID) return Promise.resolve();
-
-    return client
+  if (pupilID) {
+    client
       .query("DELETE FROM selected_option WHERE pupil_id = $1", [pupilID])
-      .then(() =>
-        client.query("DELETE FROM pupil WHERE pupil_id = $1", [pupilID])
-      );
-  };
+      .then((result) => {
+        client
+          .query("DELETE FROM pupil WHERE pupil_id = $1", [pupilID])
+          .then((result) => {
+            if (result.rowCount > 0) {
+            } else {
+              res.status(404).json({
+                result: "failure",
+                message:
+                  "Existing pupil record could not be found to be replaced",
+              });
+            }
+          })
+          .catch((error) => {
+            console.log(error.message);
+            res.status(502).json({
+              result: "failure",
+              message: "Error replacing pupil record",
+            });
+            client.end();
+          });
+      })
+      .catch((error) => {
+        console.log(error.message);
+        res.status(502).json({
+          result: "failure",
+          message: "Error replacing pupil form record",
+        });
+        client.end();
+      });
+  }
 
-  const handleError = (error, message) => {
-    if (responseSent) return;
-    console.error(error.message);
-    res.status(502).json({
-      result: "failure",
-      message: message,
-    });
-    responseSent = true;
-  };
-
-  deleteExistingRecords()
-    .then(() => client.query("SELECT MAX(pupil_id)+1 as next_id FROM pupil"))
+  // insert new pupil
+  client
+    .query("SELECT MAX(pupil_id)+1 as next_id FROM pupil")
     .then((result) => {
-      const nextPupilID = result.rows[0].next_id;
+      let nextPupilID = result.rows[0].next_id;
+      // convert updateDate into a timestamp for storing on db
       const updateTimestamp = updateDate + " 00:00:00";
-      const queryString = overrideScore
-        ? "INSERT INTO pupil (pupil_id, teacher_id, pupil_nickname, last_update, override_score, override_comment) VALUES ($1,$2,$3,$4,$5,$6)"
-        : "INSERT INTO pupil (pupil_id, teacher_id, pupil_nickname, last_update, override_comment) VALUES ($1,$2,$3,$4,$5)";
-
-      const queryArray = overrideScore
-        ? [
-            nextPupilID,
-            teacherID,
-            pupilName,
-            updateTimestamp,
-            overrideScore,
-            overrideComment,
-          ]
-        : [nextPupilID, teacherID, pupilName, updateTimestamp, overrideComment];
-
-      return client
+      if (overrideScore) {
+        queryString =
+          "INSERT INTO pupil (pupil_id, teacher_id, pupil_nickname, last_update, override_score, override_comment) VALUES ($1,$2,$3,$4,$5,$6)";
+        queryArray = [
+          nextPupilID,
+          teacherID,
+          pupilName,
+          updateTimestamp,
+          overrideScore,
+          overrideComment,
+        ];
+      } else {
+        queryString =
+          "INSERT INTO pupil (pupil_id, teacher_id, pupil_nickname, last_update, override_comment) VALUES ($1,$2,$3,$4,$5)";
+        queryArray = [
+          nextPupilID,
+          teacherID,
+          pupilName,
+          updateTimestamp,
+          overrideComment,
+        ];
+      }
+      client
         .query(queryString, queryArray)
         .then((result) => {
-          if (result.rowCount === 0) {
-            throw new Error("Pupil record could not be inserted");
+          if (result.rowCount > 0) {
+            for (let i = 0; i < teacherSelectedAnswers.length; i++) {
+              const anAnswer = teacherSelectedAnswers[i];
+              addAnAnswer(nextPupilID, anAnswer);
+            }
+            res.status(200).json({
+              pupilID: nextPupilID,
+            });
+          } else {
+            res.status(404).json({
+              result: "failure",
+              message: "Pupil record could not be inserted",
+            });
           }
-          return Promise.all(
-            teacherSelectedAnswers.map((anAnswer) =>
-              addAnAnswer(nextPupilID, anAnswer)
-            )
-          );
         })
-        .then(() => {
-          if (responseSent) return;
-          res.status(200).json({ pupilID: nextPupilID });
-          responseSent = true;
+        .catch((error) => {
+          console.log(error.message);
+          res.status(502).json({
+            result: "failure",
+            message: "Error saving form data",
+          });
+          client.end();
         });
     })
     .catch((error) => {
-      handleError(
-        error,
-        error.message.includes("pupil_id")
-          ? "Error setting next pupil id"
-          : "Database operation failed"
-      );
+      console.log(error.message);
+      res.status(502).json({
+        result: "failure",
+        message: "Error setting next pupil id",
+      });
+      client.end();
     });
 };
 
